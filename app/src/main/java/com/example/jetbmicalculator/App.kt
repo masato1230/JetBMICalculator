5a99f5f3a047e258f3f6e93efffffb1d0c4e2894
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-08-20 19:51:49 +0900

    Add hilt setup

---
+++ app/src/main/java/com/example/jetbmicalculator/App.kt
@@ -0,0 +1,7 @@
+package com.example.jetbmicalculator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -18,7 +18,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
